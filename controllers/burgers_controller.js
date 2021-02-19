558fc43667f7b6e8a86fa327b7aa888f837a9f12
Alexander Fok <foktmalexander@gmail.com>
2021-02-18 17:31:44 -0800

    Retrieve insertId info from /add. Render page for /

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,42 +4,36 @@ const burger = require('../model/burger');
 
 router.get("/", async (req, res) => {
     try {
-        const data = await burger.all()[0];
-        console.table(data);
-        res.json(data);
-        res.end();
-    } catch (err) {
+        const data = (await burger.all())[0];
+        res.render("index", {
+            burgers: data
+        })
+        
+    } catch(err) {
         console.log(`burgers_controller: ${err}`);
-        res.status(500);
-        res.end();
+        res.status(500).end();
     }
 });
 
 router.post("/add", async (req, res) => {
-    try {
-        await burger.add(req.body.name);
-        const data = await burger.all()[0];
-        console.log(data);
-        res.json(data);
-        res.end();
+    try { 
+        const result = (await burger.add(req.body.name))[0];
+        res.json({id: result.insertId});
+    
     } catch(err) {
         console.log(`burgers_controller: ${err}`);
-        res.status(500);
-        res.end();
+        res.status(500).end();     
     }
 });
 
 router.post("/devour", async (req, res) => {
     try {
-        console.log(`burgers_controller: id=${req.body.id}`)
         await burger.devour(req.body.id);
-        const data = await burger.all()[0];
-        res.json(data);
-        res.end();
+        res.status(200).end();
+
     } catch(err) {
         console.log(`burgers_controller: ${err}`);
-        res.status(500);
-        res.end();
+        res.status(500).end();
     }
 })
 
