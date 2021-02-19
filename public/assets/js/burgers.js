213f66efeee27afbef1c032379c6e4e48db2ca04
Alexander Fok <foktmalexander@gmail.com>
2021-02-18 18:18:13 -0800

    Add eventlisteners for HTTP Requests

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,33 @@
+const addBurger = document.querySelector(".add-burger");
+
+addBurger.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const data = {name: document.getElementById("bName").value};
+    fetch("/add", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(() => {
+        location.reload();
+    })
+})
+
+const devourBurgerList = document.querySelectorAll(".change-devour");
+
+devourBurgerList.forEach(el => {
+    el.addEventListener("click", (event) => {
+        const data = {id: event.target.dataset.id};
+        fetch("/devour", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then(() => {
+            location.reload();
+        });
+    });
+})
